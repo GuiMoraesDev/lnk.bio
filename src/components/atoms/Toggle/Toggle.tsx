7acf2ff7b7b13e0831bc711a6d68c55e2ac74c6b
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-03-16 14:12:52 -0300

    style: update toggle style

--- src/components/atoms/Toggle/Toggle.tsx
+++ src/components/atoms/Toggle/Toggle.tsx
@@ -14,7 +14,7 @@ export const Toggle = forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={twMerge(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-gray-50",
+      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border border-gray-300 p-px transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       className,
     )}
     {...props}
@@ -22,7 +22,7 @@ export const Toggle = forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={twMerge(
-        "pointer-events-none block h-4 w-4 rounded-full bg-blue-500 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+        "pointer-events-none block h-3.5 w-3.5 rounded-full bg-blue-500 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
       )}
     />
   </SwitchPrimitives.Root>
